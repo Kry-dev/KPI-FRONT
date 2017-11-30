08f483f6d54acfceeb10e93aecaff9c315790d35
kry-dev <yaroslavkryvda@gmail.com>
2017-11-30 17:24:27 +0200

    Roma fix footer/ Yarik fix gulp bugs

--- gulpfile.js
+++ gulpfile.js
@@ -11,9 +11,6 @@ const browserSync = require('browser-sync');
 const notify = require('gulp-notify');
 const rimraf = require('rimraf');
 const gulpWebpack = require('webpack-stream');
-const svgSprite = require('gulp-svg-sprite');
-const svgmin = require('gulp-svgmin');
-const spritesmith = require('gulp.spritesmith');
 const cheerio = require('gulp-cheerio');
 const replace = require('gulp-replace');
 const gulpStylelint = require('gulp-stylelint');
@@ -47,9 +44,8 @@ gulp.task('css', function () {
         .pipe(sourcemaps.init())
         .pipe(sass()).on('error', notify.onError({ title: 'Style' }))
         .pipe(gulpStylelint({
-            failAfterError: false,
-            ignoreFiles: "build/**/*.css",
-            reporters: [
+            "failAfterError": false,
+            "reporters": [
               {formatter: 'string', console: true}
             ]
           }))
@@ -79,14 +75,12 @@ gulp.task('watch', function() {
 
 //---------------------------------------- server ---------------------------------------//
 gulp.task('serve', function() {
-
     browserSync.init({
         //proxy: 'localhost:3000',
         open: false,
         server: './build'
 
     });
-
     browserSync.watch(['./build' + '/**/*.*', '!**/*.css'], browserSync.reload);
 });
 
@@ -105,58 +99,10 @@ gulp.task('default', gulp.series(
         'copy.fonts',
         'css',
         'webpack',
-        'pug' 
+        'pug'
     ),
     gulp.parallel(
         'watch',
         'serve'
     )
 ));
-
-//------------------svg sprites------------------------------------------------------------//
-gulp.task('sprite:svg', function() {
-    return gulp.src('./srs/images/svg-sprites/*.svg')
-      .pipe(svgmin({
-        js2svg: {
-          pretty: true
-        }
-      }))
-      .pipe(cheerio({
-        run: function ($) {
-          $('[fill]').removeAttr('fill');
-          $('[stroke]').removeAttr('stroke');
-          $('[style]').removeAttr('style');
-        },
-        parserOptions: { xmlMode: true }
-      }))
-      .pipe(replace('&gt;', '>'))
-      .pipe(svgSprite({
-        mode: {
-          symbol: {
-            sprite: "../sprite.svg"
-          }
-        }
-      }))
-      .pipe(dest('./source/images/sprite'))
-  });
-
-  // ---------------------------png sprites --------------------------------//
- gulp.task('sprite:png', function() {
-    var spriteData = gulp.src('./src/images/png-sprites/*.png')
-      .pipe(spritesmith({
-			    imgName: 'sprite.png',
-			    cssName: 'sprite.scss',
-          cssFormat: 'scss',
-          imgPath: '../images/png-sprite/sprite.png',
-          algorithm: 'binary-tree',
-			    padding: 5,
-          cssVarMap: function(sprite) {
-                    sprite.name = 's-' + sprite.name
-                }
-			  }));
-
-    spriteData.img.pipe(gulp.dest('./src/images/sprite'));
-    spriteData.css.pipe(gulp.dest('./src/styles/common'));
-
-    return spriteData;
-  });
